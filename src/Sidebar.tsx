a108b67cc90b6775855e4a77471e08287c97dc91
Lon Beshiri <lonbeshiri@gmail.com>
2023-02-07 18:59:25 -0500

    feat: sidebar close

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -4,10 +4,13 @@ import styles from './Sidebar.module.scss'
 export interface ISidebarProps {}
 
 const Sidebar: React.FC<ISidebarProps> = ({}) => {
+  const closeSidebar = () =>
+    document.documentElement.style.setProperty('--sidebar-margin', '-100%')
+
   return (
     <div className={styles.sidebar}>
       <div className={styles.close}>
-        <button>❌</button>
+        <button onClick={() => closeSidebar()}>❌</button>
       </div>
     </div>
   )
